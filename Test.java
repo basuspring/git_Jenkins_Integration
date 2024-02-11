73ec3a5fda6ed21dadba025e71912c4f677e6900
basu <prjspring@gmail.com>
2024-02-11 20:33:32 +0530

    added Test.java file for test

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+	
+	
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello this is for demo for our git-jenkins integrations");
+		
+
+	}
+
+}
